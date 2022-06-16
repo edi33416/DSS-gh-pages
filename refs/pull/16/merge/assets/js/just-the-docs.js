957c8df2a9bcb3ff52b8cf0ba4ba3befed552eae
GitHub Action <action@github.com>
2022-06-16 18:50:16 +0000

    Update "refs/pull/16/merge"

--- refs/pull/16/merge/assets/js/just-the-docs.js
+++ refs/pull/16/merge/assets/js/just-the-docs.js
@@ -53,7 +53,7 @@ function initNav() {
 
 function initSearch() {
   var request = new XMLHttpRequest();
-  request.open('GET', 'https://edi33416.github.io/dss-gh-pages/refs/pull/16/merge/assets/js/search-data.json', true);
+  request.open('GET', '/assets/js/search-data.json', true);
 
   request.onload = function(){
     if (request.status >= 200 && request.status < 400) {
@@ -430,7 +430,7 @@ jtd.getTheme = function() {
 
 jtd.setTheme = function(theme) {
   var cssFile = document.querySelector('[rel="stylesheet"]');
-  cssFile.setAttribute('href', 'https://edi33416.github.io/dss-gh-pages/refs/pull/16/merge/assets/css/just-the-docs-' + theme + '.css');
+  cssFile.setAttribute('href', '/assets/css/just-the-docs-' + theme + '.css');
 }
 
 // Document ready
